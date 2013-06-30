594ae49fd97bb57a87bd4b67f70169e45581b294
Yash Kansagara <yash.only14u@gmail.com>
2013-06-30 05:31:37 +0530

    IPaddress and Subnet Calculator

--- IPCalc/src/main/java/com/yash/ipcalc/MainActivity.java
+++ IPCalc/src/main/java/com/yash/ipcalc/MainActivity.java
@@ -17,6 +17,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -50,72 +51,155 @@ public class MainActivity extends Activity implements View.OnClickListener {
         p("On click");
         String IP_address = ((EditText)findViewById(R.id.ipet)).getText().toString();
         p("IP_address"+":"+IP_address);
-        String ip_class="";
-        if(IP_address.equals(""))
-            return;
-        ip_class = getIpClass(IP_address);
-        p(ip_class);
-        if(ip_class.equals("invalid"))
-            return;
-        ipclassans.setText(ip_class);
-        if(subnetbox.getText().toString().equals("")){
-            String subnet;
-            if (ip_class.equals("A")) {
-                subnet = "255.0.0.0";
-            } else if (ip_class.equals("B")) {
-                subnet = "255.255.0.0";
-            } else if (ip_class.equals("C")) {
-                subnet = "255.255.255.0";
-            }else{
-                subnet = "";
-            }
-            subnetbox.setText(subnet);
-        }else{
-            String subnet = subnetbox.getText().toString();
-            if(!subnet.matches("[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+"))
-                return;
 
-            String[] subnet_o = subnet.split("\\.");
-            p("Length:"+subnet_o.length);
-            p(subnet_o[0]+":"+subnet_o[1]+":"+subnet_o[2]+":"+subnet_o[3]);
-            subnet_o[0] = Integer.toBinaryString(Integer.parseInt(subnet_o[0]));
-            subnet_o[1] = Integer.toBinaryString(Integer.parseInt(subnet_o[1]));
-            subnet_o[2] = Integer.toBinaryString(Integer.parseInt(subnet_o[2]));
-            subnet_o[3] = Integer.toBinaryString(Integer.parseInt(subnet_o[3]));
-            p(subnet_o[0]+":"+subnet_o[1]+":"+subnet_o[2]+":"+subnet_o[3]);
-            String binary_subnet = subnet_o[0]+subnet_o[1]+subnet_o[2]+subnet_o[3];
-            p(binary_subnet);
-            int subnet_bits = 0;
-            while(binary_subnet.charAt(subnet_bits) == '1'){
-                subnet_bits++;
-            }
-            p("Subnet Bits:"+subnet_bits);
-            p(""+(Math.pow(2,subnet_bits%8)));
-            p(""+(Math.pow(2, 32 - subnet_bits)-2));
-            totalsubnets.setText(""+Math.pow(2,subnet_bits%8));
-            totalhosts.setText(String.format("%d",(int)(Math.pow(2, 32 - subnet_bits)-2)));
+        // Validate IP address
+        if(IP_address.equals("") || !validateIpAddress(IP_address))
+            return;
+
+        // Get IP class
+        String ip_class = getIpClass(IP_address);
+        p(ip_class);
+        ipclassans.setText(ip_class);
+
+        String subnet = subnetbox.getText().toString();
+        //if Subnet is Empty create get a default
+        if (subnet.equals("")) {
+            subnet = getDefaultSubnet(ip_class);
+            subnetbox.setText(subnet);
+            ((Button)findViewById(R.id.calculate)).performClick();
+
+            // make popup for invalid subnet and focus to subnet box
+        } else if (!validateSubnetMask(subnet,ip_class)) {
+            Toast.makeText(this,"Invalid subnet",Toast.LENGTH_LONG).show();
+            subnetbox.requestFocus();
+            clear();
+
+            // if Subnet is Valid
+        } else {
+            // Get binary format of Subnet Mask
+            String binarySubnet = getBinarySubnet(subnet);
+
+            int totalSubnets = getTotalSubnets(binarySubnet,ip_class);
+            p("" + totalSubnets);
+            totalsubnets.setText("" + totalSubnets);
+
+            int hostsPerSubnet = getHostsPerSubnet(binarySubnet);
+            p("" + hostsPerSubnet);
+            totalhosts.setText(String.format("" + hostsPerSubnet));
         }
 
     }
 
+    private void clear() {
+        totalhosts.setText("");
+        totalsubnets.setText("");
+    }
+
+    private int getTotalSubnets(String binarySubnet, String ip_class) {
+        int leastBits =8;
+        if (ip_class.equals("B")) {
+            leastBits = 16;
+
+        } else if (ip_class.equals("C")) {
+            leastBits = 24;
+
+        }
+        int numOf1s = binarySubnet.indexOf("0");
+        int subnetBits = numOf1s - leastBits;
+        return (int) Math.pow(2, subnetBits);
+    }
+
+    private String getDefaultSubnet(String ip_class) {
+        String subnet;
+        if (ip_class.equals("A")) {
+            subnet = "255.0.0.0";
+        } else if (ip_class.equals("B")) {
+            subnet = "255.255.0.0";
+        } else if (ip_class.equals("C")) {
+            subnet = "255.255.255.0";
+        }else{
+            subnet = "";
+        }
+        return subnet;
+    }
+
     private String getIpClass(String ipAddress) {
-        if(!ipAddress.matches("[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+"))
-            return "invalid";
-        int network = Integer.parseInt(ipAddress.substring(0,ipAddress.indexOf('.')));
+
+        int network = Integer.parseInt(ipAddress.substring(0, ipAddress.indexOf('.')));
         String ip_class;
 
-        if (network <= 127 && network > 0){
+        if (network <= 127 && network > 0) {
             ip_class = "A";
-        }else if(network <= 191 && network > 0){
+        } else if (network <= 191 && network > 0) {
             ip_class = "B";
-        }else if(network <=223 && network > 0){
+        } else if (network <= 223 && network > 0) {
             ip_class = "C";
-        }else{
+        } else {
             ip_class = "Reserved";
         }
         return ip_class;
     }
     public void p(String msg){
         System.out.println(msg);
+    }
+
+    public boolean validateIpAddress(String address){
+        if(!address.matches("[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+"))
+            return false;
+        String octs[] = address.split("\\.");
+        Boolean ret = true;
+        for(String x: octs) {
+            if (Integer.parseInt(x) > 255)
+                ret = false;
+        }
+        return ret;
+    }
+
+    public boolean validateSubnetMask(String subnet, String ip_class){
+        if(!validateIpAddress(subnet))
+            return false;
+        subnet = getBinarySubnet(subnet);
+        int leastBits=8;
+        if (ip_class.equals("A")) {
+            leastBits = 8;
+
+        } else if (ip_class.equals("B")) {
+            leastBits = 16;
+
+        } else if (ip_class.equals("C")) {
+            leastBits = 24;
+
+        }
+        p(subnet.indexOf("0")+"");
+        if(subnet.matches("1*0*") && subnet.indexOf("0")>=leastBits)
+            return true;
+        else
+            return false;
+    }
+
+    public int getHostsPerSubnet(String binarySubnet) {
+        return (int) Math.pow(2, 32 - binarySubnet.indexOf("0"));
+    }
+
+    public String getBinarySubnet(String subnet){
+        String[] subnet_o = subnet.split("\\.");
+        p("Length:"+subnet_o.length);
+        p(subnet_o[0]+":"+subnet_o[1]+":"+subnet_o[2]+":"+subnet_o[3]);
+        // convert to binary
+        subnet_o[0] = Integer.toBinaryString(Integer.parseInt(subnet_o[0]));
+        subnet_o[1] = Integer.toBinaryString(Integer.parseInt(subnet_o[1]));
+        subnet_o[2] = Integer.toBinaryString(Integer.parseInt(subnet_o[2]));
+        subnet_o[3] = Integer.toBinaryString(Integer.parseInt(subnet_o[3]));
+        // convert each oct of 8 bits
+        for (int i=0;i<subnet_o.length;i++){
+            while (subnet_o[i].length() != 8)
+                subnet_o[i] = "0"+subnet_o[i];
+        }
+        String s = subnet_o[0]+"."+subnet_o[1]+"."+subnet_o[2]+"."+subnet_o[3];
+        ((TextView)findViewById(R.id.subnetBinary)).setText(s);
+        String binary_subnet = subnet_o[0]+subnet_o[1]+subnet_o[2]+subnet_o[3];
+        p("Binary Subnet:"+binary_subnet);
+
+        return binary_subnet;
     }
 }
